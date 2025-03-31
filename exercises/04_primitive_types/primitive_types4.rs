8cdad44789691002dd09109305b3fcf0cb629cd2
1301yashraj <1301yashraj@gmail.com>
2025-03-31 15:25:31 +0000

    23/94

--- exercises/04_primitive_types/primitive_types4.rs
+++ exercises/04_primitive_types/primitive_types4.rs
@@ -1,5 +1,18 @@
 fn main() {
     // You can optionally experiment here.
+    let s  = String::from("Yash Raj");
+    let surname = get_last_name(&s);
+    println!("{}",surname);
+}
+
+fn get_last_name(s: &String)-> &str{
+    let bytes = s.as_bytes();
+    for (i,&item) in bytes.iter().enumerate() {
+           if item == b' '{
+            return &s[i+1..];
+           }
+    }
+     &s[..]
 }
 
 #[cfg(test)]
@@ -10,7 +23,8 @@ mod tests {
 
         // TODO: Get a slice called `nice_slice` out of the array `a` so that the test passes.
         // let nice_slice = ???
-
+        let nice_slice = &a[1..a.len()-1];
+        // println!("{nice_slice}");
         assert_eq!([2, 3, 4], nice_slice);
     }
 }
